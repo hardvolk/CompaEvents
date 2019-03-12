173b7f982f5870fd05325a073fe0317aea709199
Ibarra <cesar.ibarra@accenture.com>
2019-03-12 11:45:32 -0600

    comment functions for build

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,49 +9,58 @@ admin.initializeApp();
 // });
 
 export const normalizeUserNames = functions.https.onRequest((request, response) => {
-    admin.database().ref('/users').once('value', (snapshot) => {
-        // Loop through items
-        snapshot.forEach((element) => {
-            const user = element.val();
+    // admin.database().ref('/users').once('value', (snapshot) => {
+    //     // Loop through items
+    //     snapshot.forEach((element) => {
+    //         const user = element.val();
             
-            if (user.displayName) {
-                const fullNameArray = user.displayName.trim().split(' ');
-                switch (fullNameArray.length) {
-                    case 1:
-                        element.ref.update({ firstName: fullNameArray[0] });
-                    break;
-                    case 2:
-                        element.ref.update({ firstName: fullNameArray[0], lastName: fullNameArray[1] });
-                        break;
-                    case 3:
-                        element.ref.update({ 
-                            firstName: fullNameArray[0],
-                            lastName: fullNameArray[1] + ' ' + fullNameArray[2]
-                        });
-                        break;
-                    case 4:
-                        element.ref.update({ 
-                            firstName: fullNameArray[0] + ' ' + fullNameArray[1],
-                            lastName: fullNameArray[2] + ' ' + fullNameArray[3]
-                        });
-                        break;
+    //         if (user.displayName) {
+    //             const fullNameArray = user.displayName.trim().split(' ');
+    //             switch (fullNameArray.length) {
+    //                 case 1:
+    //                     element.ref.update({ firstName: fullNameArray[0] });
+    //                 break;
+    //                 case 2:
+    //                     element.ref.update({ firstName: fullNameArray[0], lastName: fullNameArray[1] });
+    //                     break;
+    //                 case 3:
+    //                     element.ref.update({ 
+    //                         firstName: fullNameArray[0],
+    //                         lastName: fullNameArray[1] + ' ' + fullNameArray[2]
+    //                     });
+    //                     break;
+    //                 case 4:
+    //                     element.ref.update({ 
+    //                         firstName: fullNameArray[0] + ' ' + fullNameArray[1],
+    //                         lastName: fullNameArray[2] + ' ' + fullNameArray[3]
+    //                     });
+    //                     break;
                 
-                    default:
-                        if (fullNameArray.length > 4) {
-                            element.ref.update({ 
-                                firstName: fullNameArray[0] + ' ' + fullNameArray[1],
-                                lastName: fullNameArray.slice(2, fullNameArray.length - 1).join(' ')
-                            });
-                        } else {
-                            element.ref.update({ firstName: fullNameArray[0].trim() });
-                        }
-                        break;
-                }
-            } else {
-                console.log('No name uid: ', element.key);
-            }
-        });
-        console.info('---- Loop finished!');
-        response.send('Names updated successfully!');
-    });
+    //                 default:
+    //                     if (fullNameArray.length > 4) {
+    //                         element.ref.update({ 
+    //                             firstName: fullNameArray[0] + ' ' + fullNameArray[1],
+    //                             lastName: fullNameArray.slice(2, fullNameArray.length - 1).join(' ')
+    //                         });
+    //                     } else {
+    //                         element.ref.update({ firstName: fullNameArray[0].trim() });
+    //                     }
+    //                     break;
+    //             }
+    //         } else {
+    //             console.log('No name uid: ', element.key);
+    //         }
+    //     });
+    //     console.info('---- Loop finished!');
+    //     response.send('Names updated successfully!');
+    // });
+});
+
+export const addUidToUsers = functions.https.onRequest((req, res) => {
+    // admin.database().ref('/users').once('value', (snapshot) => {
+    //     snapshot.forEach((element) => {
+    //         element.ref.update({ uid: element.key });
+    //     });
+    //     res.send('Uid added!');
+    // })
 });
