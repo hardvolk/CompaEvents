8dca7a82be9ec1df6dca998811bfa5674af37fad
Cesar <hardvolk@gmail.com>
2019-07-09 11:43:46 -0500

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBypkYNGo9qMCm7M6ZPAqicfWD_Buf0OCM',
-    authDomain: 'compa-management.firebaseapp.com',
-    databaseURL: 'https://compa-management.firebaseio.com',
-    projectId: 'compa-management',
-    storageBucket: 'compa-management.appspot.com',
-    messagingSenderId: '111051808140'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
